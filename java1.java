f7d98eb254631ed02c075905015ed558bf568416
Zomato <133211215+kevingop@users.noreply.github.com>
2024-08-06 09:42:58 +0530

    java1.java

---
+++ java1.java
@@ -0,0 +1,8 @@
+public class java1 {
+    public static void main(String[] args) {
+        System.out.println("23DIT017 - DARSHAN HOTCHANDANI");
+        int age=25;
+        System.out.println(age+" is the age of Sam.");
+    }
+}
+
